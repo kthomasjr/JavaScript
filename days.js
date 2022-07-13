12f41ae88141bdfb1f72a3c5cdd67ae8bd4b30e1
Keith <ketjr81@gmail.com>
2022-07-13 07:25:17 -0700

    update days of week

--- days.js
+++ days.js
@@ -3,11 +3,31 @@ let day;
 // still figuring out switch statements, output number of days until Saturday
 
 switch (day) {
-    case value:
-        
-        break;
-   // console.log()
-    default:
-        break;
-        // console.log()
+  case Sunday:
+    alert("Six days");
+    break;
+
+  case Monday:
+    alert("Five days");
+    break;
+
+  case Tuesday:
+    alert("Four days");
+    break;
+
+  case Wednesday:
+    alert("Three days");
+    break;
+
+  case Thursday:
+    alert("Two days");
+    break;
+
+  case Friday:
+    alert("One day");
+    break;
+
+  default:
+    alert("It is Saturday!");
+    break;
 }
