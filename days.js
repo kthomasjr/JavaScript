a3ca16e1d542fb5276bef380e61d6e8ea74147d9
Keith <ketjr81@gmail.com>
2022-07-13 08:44:54 -0700

    added getDay function

--- days.js
+++ days.js
@@ -1,30 +1,39 @@
-let day;
+
+
+function myDate() {
+  let a = new Date();
+  let r = a.getDay();
+  return r;
+  //document.getElementById("myId").innerHTML = r;
+};
+
+const day = myDate();
 
 // still figuring out switch statements, output number of days until Saturday
 
 switch (day) {
-  case Sunday:
-    alert("Six days");
+  case 0:
+    alert("Six days until Saturday");
     break;
 
-  case Monday:
-    alert("Five days");
+  case 1:
+    alert("Five days until Saturday");
     break;
 
-  case Tuesday:
-    alert("Four days");
+  case 2:
+    alert("Four days until Saturday");
     break;
 
-  case Wednesday:
-    alert("Three days");
+  case 3:
+    alert("Three days until Saturday");
     break;
 
-  case Thursday:
-    alert("Two days");
+  case 4:
+    alert("Two days until Saturday");
     break;
 
-  case Friday:
-    alert("One day");
+  case 5:
+    alert("One day until Saturday");
     break;
 
   default:
