6cd3b514bbd0aaf72ea5da03e61ad3a1178e58a4
Keith <ketjr81@gmail.com>
2022-07-13 07:18:47 -0700

    switch statement

---
+++ time.js
@@ -0,0 +1,13 @@
+let day;
+
+// still figuring out switch statements, output number of days until Saturday
+
+switch (day) {
+    case value:
+        
+        break;
+   // console.log()
+    default:
+        break;
+        // console.log()
+}
